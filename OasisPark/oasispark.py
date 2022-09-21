75b07c7b5f04de86810c9ebd3d4673ff407b4c27
stigmandroid <stigmandroid@gmail.com>
2022-09-21 12:00:25 -0300

    Correção do IP
    - Corrigido endereço IP para conexão com o banco no docker.

--- OasisPark/oasispark.py
+++ OasisPark/oasispark.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
 app.config['MYSQL_DATABASE_DB'] = 'oasis'
-app.config['MYSQL_DATABASE_HOST'] = '172.17.0.0'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
 

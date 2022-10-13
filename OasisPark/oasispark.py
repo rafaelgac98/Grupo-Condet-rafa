f7b748232a5d56ac198a51b5411f791fedbf94af
rafaelgac98 <80112965+rafaelgac98@users.noreply.github.com>
2022-10-13 20:08:36 -0300

    Update oasispark.py

--- OasisPark/oasispark.py
+++ OasisPark/oasispark.py
@@ -371,5 +371,5 @@ def gravarveiculo():
 #     return render_template('cadastroveiculo.html', datas=data, pk = pk)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5005))
+    port = int(os.environ.get("PORT", 5008))
     app.run(host='0.0.0.0', port=port)
